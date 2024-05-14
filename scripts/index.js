47c25fde256acfe41c14c8920cea6c923577147f
ANTON KIREEV <AntiANT8406@yandex.ru>
2024-05-14 20:49:02 +0300

    add render function and remove default delete function in createCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const cardTemplate = document.querySelector("#card-template").content;
+const placesElement = document.querySelector('.places__list');
 
 const deleteCard = (evt) => {
     const eventTarget = evt.target;
@@ -6,10 +7,11 @@ const deleteCard = (evt) => {
     currentCard.remove();
 }
 
-const createCard = (name, link, deleteFunction = deleteCard) => {
+const createCard = (name, link, deleteFunction) => {
     const currentCard = cardTemplate.cloneNode(true);
     const cardImage = currentCard.querySelector('.card__image');
     cardImage.src = link;
+    cardImage.alt = 'Фотография ' + name;
     const cardTitle = currentCard.querySelector('.card__title');
     cardTitle.textContent = name;
     const cardDeleteButton = currentCard.querySelector('.card__delete-button');
@@ -17,10 +19,11 @@ const createCard = (name, link, deleteFunction = deleteCard) => {
     return currentCard;
 }
 
-
-const placesElement = document.querySelector('.places__list');
-console.log('started');
-for (const cardItems of initialCards) {
-    const card = createCard(cardItems.name, cardItems.link);
-    placesElement.append(card);
+const renderInitCards = (cards, deleteFunction) => {
+    for (const cardItems of cards) {
+        const card = createCard(cardItems.name, cardItems.link, deleteFunction);
+        placesElement.append(card);
+    }
 }
+
+renderInitCards(initialCards, deleteCard);
