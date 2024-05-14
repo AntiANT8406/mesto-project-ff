b25462eae52217a68f00705c7ef56ba8af4eae1e
Kireev Anton <AntiANT8406@yandex.ru>
2024-05-14 15:31:04 +0300

    initial

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+const createCard = (name, link, deleteCard) => {
+    const currentCard = cardTemplate.cloneNode(true);
+    const cardImage = currentCard.querySelector('.card__image');
+    cardImage.src = link;
+    const cardTitle = currentCard.querySelector('.card__title');
+    cardTitle.textContent = name;
+    const deleteButton = currentCard.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+    return currentCard;
+}
 
-// @todo: Функция создания карточки
+const deleteCard = (evt) => {
+    const eventTarget = evt.target;
+    const currentCard = eventTarget.closest('.card');
+    currentCard.remove();
+}
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+const placesElement = document.querySelector('.places__list');
+console.log('started');
+for (const cardItems of initialCards) {
+    const card = createCard(cardItems.name, cardItems.link);
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+    placesElement.append(card);
+}
