f0a17e9f748147df13d80d8baaef2f706b6be97c
ANTON KIREEV <AntiANT8406@yandex.ru>
2024-06-07 12:19:24 +0300

    add close profile with click

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,3 +1,14 @@
 export function openModal(modal) {
     modal.classList.add('popup_is-opened');
+    modal.addEventListener('click', closeModalWithClick)
 }
+
+function closeModalWithClick(event) {
+    const target = event.target;
+    const modal = event.currentTarget;
+    if (target.classList.contains('popup__close') || target === modal) {
+        modal.classList.remove('popup_is-opened');
+        modal.removeEventListener('click', closeModalWithClick);
+    }
+}
+
