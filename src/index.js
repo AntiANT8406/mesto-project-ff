0045c8a761fdb1baed3f04b68fdc3437be77a59f
ANTON KIREEV <AntiANT8406@yandex.ru>
2024-06-07 12:10:46 +0300

    add open profile modal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import './pages/index.css';
 import {renderInitCards} from './scripts/cards.js';
 import {createCard, deleteCard} from "./scripts/card";
+import {openModal} from "./scripts/modal";
 
 const cardTemplate = document.querySelector("#card-template").content;
 const placesElement = document.querySelector('.places__list');
 placesElement.addEventListener('click', function cardClickHandler(event) {
     const target = event.target;
-    if (target.classList.contains('card__delete-button')) {deleteCard(target.closest('.card'));}
+    if (target.classList.contains('card__delete-button')) {
+        deleteCard(target.closest('.card'));
+    }
 })
 
 renderInitCards(cardTemplate, placesElement, createCard, deleteCard);
+
+const modalProfile = document.querySelector('.popup_type_edit');
+const mainPage = document.querySelector('.content');
+mainPage.addEventListener('click', function profileClickHandler(event) {
+    const target = event.target;
+    if (target.classList.contains('profile__edit-button')) {
+        openModal(modalProfile);
+    }
+})
