bbea3988ca01aa82b042b666bf0f5eac6c5d8ccb
ANTON KIREEV <AntiANT8406@yandex.ru>
2024-06-10 20:58:09 +0300

    fixed card.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./pages/index.css";
 import { initialCards } from "./scripts/cards.js";
-import { createCard, deleteCard, likeCard, zoomCard } from "./scripts/card";
+import { createCard, deleteCard, likeCard } from "./scripts/card";
 import {
   openModal,
   closeModal,
@@ -14,6 +14,7 @@ const profileElement = document.querySelector(".profile");
 const placesElement = document.querySelector(".places__list");
 const profileModal = document.querySelector(".popup_type_edit");
 const cardAddModal = document.querySelector(".popup_type_new-card");
+const cardZoomModal = document.querySelector(".popup_type_image");
 const profileEditButton = profileElement.querySelector(".profile__edit-button");
 const profileTitle = profileElement.querySelector(".profile__title");
 const profileDescription = profileElement.querySelector(
@@ -24,10 +25,17 @@ const popupElements = document.querySelectorAll(".popup");
 const addCardButton = profileElement.querySelector(".profile__add-button");
 const addCardForm = document.forms["new-place"];
 
+function zoomCard(name, link) {
+  const imageElement = cardZoomModal.querySelector(".popup__image");
+  imageElement.src = link;
+  imageElement.alt = "Фотография " + name;
+  cardZoomModal.querySelector(".popup__caption").textContent = name;
+  openModal(cardZoomModal);
+}
+
 for (let cardItems of initialCards) {
   const card = createCard(
-    cardItems.name,
-    cardItems.link,
+    cardItems,
     cardTemplate,
     deleteCard,
     likeCard,
@@ -63,8 +71,7 @@ addCardButton.addEventListener("click", () => {
 addCardForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const card = createCard(
-    addCardForm["place-name"].value,
-    addCardForm["link"].value,
+    { name: addCardForm["place-name"].value, link: addCardForm["link"].value },
     cardTemplate,
     deleteCard,
     likeCard,
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,36 +1,31 @@
-import {openModal} from "./modal";
-
 export function deleteCard(event) {
-    const card = event.target.closest('.card');
-    card.remove();
+  const card = event.target.closest(".card");
+  card.remove();
 }
 
 export function likeCard(event) {
-    event.target.classList.toggle('card__like-button_is-active');
+  event.target.classList.toggle("card__like-button_is-active");
 }
 
-export function zoomCard(event) {
-    const popupImageElement = document.querySelector('.popup_type_image');
-    const cardElement = event.target.closest('.card');
-    const imageCaption = cardElement.querySelector('.card__title').textContent;
-    const imageLink = cardElement.querySelector('.card__image').src;
-    const imageElement = popupImageElement.querySelector('.popup__image')
-    imageElement.src = imageLink;
-    popupImageElement.querySelector('.popup__caption').textContent = imageCaption;
-    openModal(popupImageElement);
-}
-
-export function createCard(name, link, cardTemplate, deleteFunction, likeFunction, zoomFunction) {
-    const currentCard = cardTemplate.cloneNode(true);
-    const cardImage = currentCard.querySelector('.card__image');
-    cardImage.src = link;
-    cardImage.alt = 'Фотография ' + name;
-    const cardTitle = currentCard.querySelector('.card__title');
-    cardTitle.textContent = name;
-    const cardDeleteButton = currentCard.querySelector('.card__delete-button');
-    cardDeleteButton.addEventListener('click', deleteFunction);
-    const cardLikeButton = currentCard.querySelector('.card__like-button');
-    cardLikeButton.addEventListener('click', likeFunction);
-    cardImage.addEventListener('click', zoomFunction);
-    return currentCard;
+export function createCard(
+  { name, link },
+  cardTemplate,
+  deleteFunction,
+  likeFunction,
+  zoomFunction
+) {
+  const currentCard = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = currentCard.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = "Фотография " + name;
+  const cardTitle = currentCard.querySelector(".card__title");
+  cardTitle.textContent = name;
+  const cardDeleteButton = currentCard.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", deleteFunction);
+  const cardLikeButton = currentCard.querySelector(".card__like-button");
+  cardLikeButton.addEventListener("click", likeFunction);
+  cardImage.addEventListener("click", () => {
+    zoomFunction(name, link);
+  });
+  return currentCard;
 }
